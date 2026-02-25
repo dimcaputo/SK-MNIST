c02d9ee4aedd4ff5d7c4a2ff3b1fdae20e11afe6
Dimitri Caputo <dim.caputo@gmail.com>
2026-02-25 22:58:39 +0100

    move notebook to script

---
+++ helper_functions.py
@@ -0,0 +1,104 @@
+import numpy as np
+from PIL import Image
+from keras.callbacks import EarlyStopping
+from keras.utils import to_categorical
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import os
+
+def array_from_images(folder, df_metadata, dict_of_labels, h=224, w=224, channels=3):
+    # Create an array of images and labels the size of the number of pictures
+    nb_files = 0
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            nb_files += 1
+    array = np.zeros(shape=(nb_files, h, w, channels))
+    labels = np.zeros(shape=(nb_files,))
+
+    # Check the name and fill array and labels
+    df_metadata = df_metadata.set_index('image_id', drop=True)
+    count = 0
+
+    # In case of binary use
+    class_mapping = {
+        "akiec": "canc",
+        "bcc": "canc",
+        "bkl": "nocanc",
+        "df": "nocanc",
+        "mel": "canc",
+        "nv": "nocanc",
+        "vasc": "nocanc"
+    }
+
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            with Image.open(os.path.join(root, file)) as im:
+                array[count,:,:,:] = np.asarray(im.resize((h,w)))
+                try:
+                    labels[count] = dict_of_labels[class_mapping[df_metadata.loc[file.strip('.jpg'), 'dx']]]
+                except:
+                    labels[count] = dict_of_labels[df_metadata.loc[file.strip('.jpg'), 'dx']]
+                count += 1
+                if count%100 == 0:
+                    print(f'{count} images were processed')
+    return array, labels
+
+def get_earlystopping(patience=10):
+    early_stopping = EarlyStopping(
+    monitor='val_f1_score',
+    mode='max',
+    patience=patience,
+    verbose=1,
+    restore_best_weights=True)
+    return early_stopping
+
+def plot_learning_curves(model):
+    fig, ax = plt.subplots(1,2, figsize=(15,5))
+    ax[0].plot(model.history.history['val_f1_score'], label='val_f1_score')
+    ax[0].plot(model.history.history['f1_score'], label='f1_score')
+    ax[0].legend()
+    ax[1].plot(model.history.history['val_loss'], label='val_loss')
+    ax[1].plot(model.history.history['loss'], label='loss')
+    ax[1].legend()
+    fig.show;
+
+def get_analysis_cat(model, testX, testy):
+    plot_learning_curves(model)
+    loss, f1 = model.evaluate(testX, testy)
+    print(f'The model gave')
+    print(f'Loss: {loss:.2f}')
+    print(f'F1 Macro: {f1:.2f}')
+    # print(f'Avg Prec: {avg_prec:.2f}')
+    predy = model.predict(testX)
+    resy = to_categorical(np.argmax(predy, axis=1), num_classes=7)
+    print(classification_report(testy,resy))
+    return predy, resy
+
+def compile_and_train(model, loss, opt, metrics, epochs, patience=None, steps=None, class_weight=None):
+    model.compile(loss=loss,
+                optimizer=opt,
+                metrics=metrics)
+
+    model.summary()
+
+    if patience != None:
+        model.fit(
+            X_train,
+            y_train,
+            epochs=epochs,
+            validation_data=(X_val, y_val),
+            callbacks=[get_earlystopping(patience)],
+            steps_per_epoch=steps,
+            class_weight=class_weight
+            )
+    else:
+        model.fit(
+            X_train,
+            y_train,
+            epochs=epochs,
+            validation_data=(X_val, y_val),
+            steps_per_epoch=steps,
+            class_weight=class_weight
+            )
+
+    return model
--- main.py
+++ main.py
@@ -1,6 +1,103 @@
-def main():
-    print("Hello from sk-mnist!")
+import warnings
+warnings.filterwarnings('ignore')
+import os
+os.environ["KERAS_BACKEND"] = "torch"
+import pandas as pd
+import torch
+from keras.layers import Input, Rescaling, Dropout
+from keras.layers import RandomFlip, RandomTranslation, RandomRotation, RandomZoom
+from keras.models import Sequential
+from keras.metrics import F1Score
+from keras.losses import CategoricalCrossentropy
+from keras.utils import to_categorical
+from keras.optimizers.schedules import CosineDecay
+from keras.optimizers import AdamW
+from keras.applications import EfficientNetV2B0
+from sklearn.model_selection import train_test_split
 
+from helper_functions import *
+import argparse
 
-if __name__ == "__main__":
-    main()
+class_mapping = {
+        "akiec": "canc",
+        "bcc": "canc",
+        "bkl": "nocanc",
+        "df": "nocanc",
+        "mel": "canc",
+        "nv": "nocanc",
+        "vasc": "nocanc"
+    }
+
+if __name__ == "_main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--size", default=224, type=int)
+    parser.add_argument("--load_dataset", action="store_true")
+    parser.add_argument("--learning_rate", default=1e-4, type=float)
+    parser.add_argument("--patience", default=50, type=int)
+    parser.add_argument("--epochs", default=500, type=int)
+    parser.add_argument("--output_name", default="trained_model.pth", type=str)
+    args = parser.parse_args()
+
+    df = pd.read_csv('HAM10000_metadata.csv')
+    dict_label = {k:v for k,v in zip(list(class_mapping.keys()), range(7))}
+
+    if args.load_dataset == True:
+        X, y = array_from_images('dataset/', df, dict_label, h=args.size, w=args.size)
+        np.savez_compressed(f'X-{args.size}x{args.size}', X, allow_pickle=True)
+        np.savez_compressed(f'y-{args.size}x{args.size}', y, allow_pickle=True)
+
+    X = np.load(f'X-{args.size}x{args.size}.npz')['arr_0']
+    y = np.load(f'y-{args.size}x{args.size}.npz')['arr_0']
+
+    class_weights = {i:(len(y)/(7 * sum(y==i))).item() for i in range(7)}
+
+    X_train, X, y_train, y = train_test_split(X, y, stratify=y, test_size=0.3, random_state=38)
+    X_test, X_val, y_test, y_val = train_test_split(X, y, stratify=y, test_size=0.5, random_state=38)
+
+    del X,y
+
+    y_train = to_categorical(y_train, num_classes=7)
+    y_val = to_categorical(y_val, num_classes=7)
+    y_test = to_categorical(y_test, num_classes=7)
+
+    effnet = EfficientNetV2B0(
+        include_top=True,
+        input_shape=X_train.shape[1:],
+        weights=None,
+        classes=7,
+        classifier_activation="softmax",
+    )
+
+    preproc = Sequential([
+        RandomFlip(),
+        RandomRotation(factor=0.5),
+        RandomTranslation(height_factor=0.5, width_factor=0.5),
+        RandomZoom(height_factor=0.5, width_factor=0.5)
+    ])
+
+    model = Sequential([
+        Input(shape=(X_train.shape[1:])),
+        Rescaling(scale=1./255),
+        preproc,
+        effnet, 
+    ])
+
+    lr_schedule = CosineDecay(initial_learning_rate=1e-4, decay_steps=500, alpha=0.1) 
+    optimizer = AdamW(learning_rate=lr_schedule, weight_decay=0.01)
+
+    history020 = compile_and_train(model,
+                                loss=CategoricalCrossentropy(),
+                                opt=optimizer,
+                                metrics=[F1Score(average='macro')],
+                                epochs=args.epochs,
+                                patience=args.patience, 
+                                class_weight=class_weights
+                    )
+
+    try:
+        pred, res = get_analysis_cat(model, X_test, y_test)
+    except:
+        pass
+
+    torch.save(model, f"{args.output_name}")
