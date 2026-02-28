90fbaf8541196b1109a1cb799c696f687a0490b8
Dimitri Caputo <dim.caputo@gmail.com>
2026-02-28 17:50:17 +0100

    modified to accomodate different metrics

--- helper_functions.py
+++ helper_functions.py
@@ -37,15 +37,16 @@ def array_from_images(folder, df_metadata, dict_of_labels, h=224, w=224, channel
                 try:
                     labels[count] = dict_of_labels[class_mapping[df_metadata.loc[file.strip('.jpg'), 'dx']]]
                 except:
-                    labels[count] = dict_of_labels[df_metadata.loc[file.strip('.jpg'), 'dx']]
+                    raise TypeError('NOPE')
+                    # labels[count] = dict_of_labels[df_metadata.loc[file.strip('.jpg'), 'dx']]
                 count += 1
                 if count%100 == 0:
                     print(f'{count} images were processed')
     return array, labels
 
-def get_earlystopping(patience=10):
+def get_earlystopping(metric_name, patience=10):
     early_stopping = EarlyStopping(
-    monitor='val_f1_score',
+    monitor=f'val_{metric_name}',
     mode='max',
     patience=patience,
     verbose=1,
@@ -54,9 +55,14 @@ def get_earlystopping(patience=10):
 
 def plot_learning_curves(model):
     fig, ax = plt.subplots(1,2, figsize=(15,5))
-    ax[0].plot(model.history.history['val_f1_score'], label='val_f1_score')
-    ax[0].plot(model.history.history['f1_score'], label='f1_score')
-    ax[0].legend()
+    try:
+        ax[0].plot(model.history.history['val_f1_score'], label='val_f1_score')
+        ax[0].plot(model.history.history['f1_score'], label='f1_score')
+        ax[0].legend()
+    except:
+        ax[0].plot(model.history.history['val_avg_prec'], label='val_avg_prec')
+        ax[0].plot(model.history.history['avg_prec'], label='avg_prec')
+        ax[0].legend()
     ax[1].plot(model.history.history['val_loss'], label='val_loss')
     ax[1].plot(model.history.history['loss'], label='loss')
     ax[1].legend()
@@ -74,6 +80,18 @@ def get_analysis_cat(model, testX, testy):
     print(classification_report(testy,resy))
     return predy, resy
 
+def get_analysis(model, testX, testy):
+    plot_learning_curves(model)
+    loss, avg_prec = model.evaluate(testX, testy)
+    print(f'The model gave')
+    print(f'Loss: {loss:.2f}')
+    # print(f'F1 Macro: {f1:.2f}')
+    print(f'Avg Prec: {avg_prec:.2f}')
+    predy = model.predict(testX)
+    resy = (predy > 0.5).astype(int)
+    print(classification_report(testy,resy))
+    return predy, resy
+
 def compile_and_train(model, train_data, val_data, loss, opt, metrics, epochs, patience=None, steps=None, class_weight=None):
     model.compile(loss=loss,
                 optimizer=opt,
@@ -87,7 +105,7 @@ def compile_and_train(model, train_data, val_data, loss, opt, metrics, epochs, p
             train_data[1],
             epochs=epochs,
             validation_data=(val_data[0], val_data[1]),
-            callbacks=[get_earlystopping(patience)],
+            callbacks=[get_earlystopping(metrics[0].name, patience)],
             steps_per_epoch=steps,
             class_weight=class_weight
             )
