e0e2204594c47bff8ccc93f9351c5ecfc8764d47
Dimitri Caputo <dim.caputo@gmail.com>
2026-02-28 17:49:57 +0100

    changes for binary training with AUC PR instead of f1

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import torch
 from keras.layers import Input, Rescaling, Dropout
 from keras.layers import RandomFlip, RandomTranslation, RandomRotation, RandomZoom
 from keras.models import Sequential
-from keras.metrics import F1Score
+from keras.metrics import F1Score, AUC
 from keras.losses import CategoricalCrossentropy, BinaryFocalCrossentropy
 from keras.utils import to_categorical
 from keras.optimizers.schedules import CosineDecay
 from keras.optimizers import AdamW
-from keras.applications import EfficientNetV2B0, VGG16, Xception, MobileNetV2, DenseNet121, ConvNeXtTiny, NASNetMobile
+from keras.applications import EfficientNetV2B0, MobileNetV2, DenseNet121, NASNetMobile
 from sklearn.model_selection import train_test_split
+
 
 from helper_functions import *
 import argparse
@@ -30,13 +31,13 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(prog="main.py")
     parser.add_argument("--size", default=224, type=int)
-    parser.add_argument("--label_type", choices=["diseases", "cancer"])
-    parser.add_argument("--make_dataset", action="store_true")
-    parser.add_argument("--learning_rate", default=1e-4, type=float)
+    parser.add_argument("--label-type", choices=["diseases", "cancer"])
+    parser.add_argument("--make-dataset", action="store_true")
+    parser.add_argument("--learning-rate", default=1e-4, type=float)
     parser.add_argument("--patience", default=50, type=int)
     parser.add_argument("--epochs", default=500, type=int)
-    parser.add_argument("--output_name", default="trained_model.pth", type=str)
-    parser.add_argument("--model", choices=["EfficientNetV2B0", "VGG16", "Xception", "MobileNetV2", "DenseNet121", "ConvNeXtTiny", "NASNetMobile"])
+    parser.add_argument("--output-name", default="trained_model.pth", type=str)
+    parser.add_argument("--model", choices=["EfficientNetV2B0", "MobileNetV2", "DenseNet121", "NASNetMobile"])
     args = parser.parse_args()
 
     df = pd.read_csv('HAM10000_metadata.csv')
@@ -49,12 +50,13 @@ if __name__ == "__main__":
     if args.make_dataset == True:
         print("Making the dataset...")
         X, y = array_from_images('dataset/', df, dict_label, h=args.size, w=args.size)
+        print("Saving the dataset...")
         np.savez_compressed(f'X-{args.size}x{args.size}_{args.label_type}', X, allow_pickle=True)
         np.savez_compressed(f'y-{args.size}x{args.size}_{args.label_type}', y, allow_pickle=True)
-
-    print("Loading the dataset...")
-    X = np.load(f'X-{args.size}x{args.size}.npz')['arr_0']
-    y = np.load(f'y-{args.size}x{args.size}.npz')['arr_0']
+    else:
+        print("Loading the dataset...")
+        X = np.load(f'X-{args.size}x{args.size}_{args.label_type}.npz')['arr_0']
+        y = np.load(f'y-{args.size}x{args.size}_{args.label_type}.npz')['arr_0']
 
     if args.label_type == "diseases":
         class_weights = {i:(len(y)/(7 * sum(y==i))).item() for i in range(7)}
@@ -66,27 +68,23 @@ if __name__ == "__main__":
     
     match args.label_type:
         case "diseases":
-            y_train = to_categorical(y_train, num_classes=7)
-            y_val = to_categorical(y_val, num_classes=7)
-            y_test = to_categorical(y_test, num_classes=7)
-
-            model = eval(args.model)(
-                include_top=True,
-                input_shape=X_train.shape[1:],
-                weights=None,
-                classes=7,
-                classifier_activation="softmax",
-            )
+            num_classes=7
+            activation="softmax",
+            y_train = to_categorical(y_train, num_classes=num_classes)
+            y_val = to_categorical(y_val, num_classes=num_classes)
+            y_test = to_categorical(y_test, num_classes=num_classes)
         case "cancer":
-            model = eval(args.model)(
-                include_top=True,
-                input_shape=X_train.shape[1:],
-                weights=None,
-                classes=1,
-                classifier_activation="sigmoid",
-            )
+            num_classes=1
+            activation="sigmoid"
     
-
+    model = eval(args.model)(
+        include_top=True,
+        input_shape=X_train.shape[1:],
+        weights=None,
+        classes=num_classes,
+        classifier_activation=activation,
+    )    
+    
     preproc = Sequential([
         RandomFlip(),
         RandomRotation(factor=0.5),
@@ -128,13 +126,13 @@ if __name__ == "__main__":
                 val_data = (X_val, y_val),
                 loss=BinaryFocalCrossentropy(),
                 opt=optimizer,
-                metrics=[F1Score(average='macro')],
+                metrics=[AUC(curve='PR', name='average_precision')],
                 epochs=args.epochs,
                 patience=args.patience, 
                 class_weight=None
             )
             try:
-                pred, res = get_analysis_cat(model, X_test, y_test)
+                pred, res = get_analysis(model, X_test, y_test)
             except:
                 pass
 
